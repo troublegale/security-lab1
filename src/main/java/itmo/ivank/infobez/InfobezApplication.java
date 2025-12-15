ddd4deebeb0e0960c02906ad82496ada2d9b632a
ivank <367293@edu.itmo.ru>
2025-12-15 14:45:19 +0300

    initial commit

---
+++ src/main/java/itmo/ivank/infobez/InfobezApplication.java
@@ -0,0 +1,13 @@
+package itmo.ivank.infobez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfobezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfobezApplication.class, args);
+	}
+
+}
